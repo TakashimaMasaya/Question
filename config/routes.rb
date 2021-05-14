f33a0f9437d313407df5c5baf9d55f1fa7b36164
3469masa <3469masa@gmail.com>
2021-05-14 17:51:06 +0800

    #

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-
-  # petch 'answered(/:id)', to: 'quests#answered'
   
 
   namespace :admin do
@@ -14,10 +12,6 @@ Rails.application.routes.draw do
   root to: 'quests#index'
   resources :quests do 
     resources :answers
-    # member do
-    #   patch 'answered', to: 'quests#answered'
-    #   patch 'unanswered', to: 'quests#unanswered'
-    # end
     get 'quests/answered' => 'quests#answered'
     get 'quests/unanswered' => 'quests#unanswered'
   end 
